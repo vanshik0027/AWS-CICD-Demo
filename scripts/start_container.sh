c7ea61dc886cc19a57ef7bab5982a55071e72b52
vanshik0027 <100398408+vanshik0027@users.noreply.github.com>
2024-05-15 20:18:24 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull vanshik123docker/cicd-demo-aws
+sudo docker pull vanshik123docker/cicd-demo-aws
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 vanshik123docker/cicd-demo-aws
+sudo docker run -d -p 5000:5000 vanshik123docker/cicd-demo-aws
