9c77d58f246a078fc440d8051e5c96b603d1d27a
vanshik0027 <100398408+vanshik0027@users.noreply.github.com>
2024-05-15 19:17:54 +0530

    add file for deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull vanshik123docker/cicd-demo-aws
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 vanshik123docker/cicd-demo-aws
